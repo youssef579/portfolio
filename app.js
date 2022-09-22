91f0cfee833ad15d5616a04ef5196c92447252ea
Youssef Ragaey <youssefragaa3@gmail.com>
2022-09-22 17:51:21 +0200

    Fix bugs

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ let header = document.querySelector("header");
 var scrolling = scrollY;
 
 function checkResize() {
-    // document.querySelectorAll(".projects .main-projects .project:nth-of-type(odd) .description").forEach(function (element) {
-
-    // });
-
     if (window.innerWidth <= 767) {
         document.querySelectorAll(".projects .main-projects article").forEach(function (x) {
             if (x.nextElementSibling !== null)
@@ -19,7 +15,7 @@ function checkResize() {
     }
 }
 
-checkResize()
+checkResize();
 
 list.addEventListener("click", function () {
     nav.style.right = "0";
